e60f7d5ef1b71aac384f62cb00de5f583b455e4d
David Santiago <david.santiago@gmail.com>
2011-05-23 19:12:39 -0500

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject mustachequerade "0.0.1-SNAPSHOT"
+  :description "TODO: add summary of your project"
+  :dependencies [[clojure "1.2.0"]
+                 [stencil "0.1.0"]
+                 [com.github.fhd.clostache/clostache "0.4.1"]
+                 [mustache "0.1.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clj "0.1.6-SNAPSHOT"]]
+  :tasks [cake.tasks.swank-clj])
