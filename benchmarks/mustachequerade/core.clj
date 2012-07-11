649f3e40351b1b4baecc583826d860f3d873d331
David Santiago <david.santiago@gmail.com>
2012-07-11 15:22:46 -0700

    Rewrite to use perforate.

---
+++ benchmarks/mustachequerade/core.clj
@@ -0,0 +1,59 @@
+(ns mustachequerade.core
+  (:use perforate.core)
+  (:require [stencil.core :as stencil]
+            [stencil.loader :as stenload]
+            [clostache.parser :as clostache]
+            [me.shenfeng.mustache :as shenmustache]))
+
+
+(defgoal main-bench "Benchmark the template called main.")
+
+(def main-data {:projects [{:name "resque"
+                            :url "http://github.com/defunkt/resque"
+                            :description "A Redis-backed Ruby library for queueing and working."}]
+                             :width 4968})
+
+(defcase main-bench :stencil
+  []
+  (let [cached-template (stenload/load "templates/main")]
+    (stencil/render cached-template
+                    main-data)))
+
+(defcase* main-bench :clostache
+  (fn []
+    (let [main-src (slurp "resources/templates/main.mustache")]
+      [(fn [] (clostache/render main-src main-data))])))
+
+(defcase* main-bench :shenmustache
+  (fn []
+    (shenmustache/deftemplate cached-template
+      (slurp "resources/templates/main.mustache"))
+    [(fn [] (shenmustache/to-html cached-template main-data))]))
+
+;;
+;; Demo benchmark
+;;
+
+(defgoal demo-bench "Benchmark the template called demo.")
+
+(def demo-data {:header "Colors"
+                :items [{:name "red" :first true :url "#Red"}
+                        {:name "green" :link true :url "#Green"}
+                        {:name "blue" :link  true :url "#Blue"}]
+                :empty false})
+
+(defcase demo-bench :stencil
+  []
+  (let [cached-template (stenload/load "templates/demo")]
+    (stencil/render cached-template demo-data)))
+
+(defcase* demo-bench :clostache
+  (fn []
+    (let [demo-src (slurp "resources/templates/demo.mustache")]
+      [(fn [] (clostache/render demo-src demo-data))])))
+
+(defcase* demo-bench :shenmustache
+  (fn []
+    (shenmustache/deftemplate demo-template
+      (slurp "resources/templates/demo.mustache"))
+    [(fn [] (shenmustache/to-html demo-template demo-data))]))
--- project.clj
+++ project.clj
@@ -1,7 +1,14 @@
 (defproject mustachequerade "0.0.2-SNAPSHOT"
   :description "TODO: add summary of your project"
-  :dependencies [[clojure "1.3.0"]
-                 [stencil "0.2.0"]
-                 [de.ubercode.clostache/clostache "0.6.0"]
-                 [mustache "0.1.0-SNAPSHOT"]]
-  :dev-dependencies [[ritz "0.2.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [stencil "0.3.0-SNAPSHOT"]
+                 [de.ubercode.clostache/clostache "1.3.0"]
+                 [mustache "0.1.0-SNAPSHOT"]
+                 [me.shenfeng/mustache "0.0.3"]]
+
+  :plugins [[perforate "0.2.3"]]
+  :profiles {:current {:dependencies [[org.clojure/clojure "1.4.0"]]}}
+  :perforate {:environments [{:name :main
+                              :profiles [:current]
+                              :namespaces [mustachequerade.core]}]})
+
